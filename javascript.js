969d963f7c9b2fd5a262fef701fd09ffaf604018
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-05-04 14:03:15 -0300

    select

--- javascript.js
+++ javascript.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+	
+	$.getJSON('http://192.168.1.109:8080/list', function(data){
+		var out='';
+		for (var c=0;c<data.length;c++){
+			out+='<option value='+data[c].chave+'>'+data[c].nome+'</option>';
+		}
+		$('#selecionar').html(out);
+		$('#submit').click(function(){
+			var i=$('#selecionar').val();
+			var result='';
+			result+='Nome: ' + data[i-1].nome + '<br />';
+			result+='Valor: ' + data[i-1].valor + '<br />';
+			result+='Status: ' + data[i-1].status + '<br />';
+			result+='Estoque: ' + data[i-1].estoque + '<br />';
+			$('#conteudo').html(result);
+		});
+	});
+	/*
 	$('#submit').click(function(){
 		$.getJSON('http://192.168.1.109:8080/list', function(data){
 			var i = $('#products').val();
@@ -7,7 +25,7 @@ $(document).ready(function(){
 			result+='Valor: ' + data[i].valor + '<br />';
 			result+='Status: ' + data[i].status + '<br />';
 			result+='Estoque: ' + data[i].estoque + '<br />';
-			$('#conteud').html(result);
+			$('#conteudo').html(result);
 		});
-	});
+	});*/
 });
