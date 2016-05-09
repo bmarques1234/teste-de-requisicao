875a41c6548c5bda4ade7f149e0a4d03bc025d0a
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-05-09 13:10:49 -0300

    identação

--- app/index/javascript.js
+++ app/index/javascript.js
@@ -1,9 +1,7 @@
 function att(data){
 	var options='<option selected value="">Selecione um produto...</option><option value=0>todos</option>';
 	for (var c=0;c<data.length;c++){
-		if(data[c].status == 'I'){
-			options+='<option value='+data[c].chave+' style="color:red">'+data[c].nome+'</option>';
-		}
+		if(data[c].status == 'I') options+='<option value='+data[c].chave+' style="color:red">'+data[c].nome+'</option>';
 		else options+='<option value='+data[c].chave+'>'+data[c].nome+'</option>';
 	}
 	$('#selecionar').html(options);
@@ -25,14 +23,11 @@ function show(allData,url){
 		else{
 			result+='<tr>'+'<td>'+data.nome+'</td>'+'<td>R$ '+data.valor+'</td>';
 			result+='<td>'+data.status+'</td>'+'<td>'+data.estoque+'</td>'+'</tr>';
-			if(data.status == 'I'){
-				result+='<tr><td colspan="4" style="color:red">Este produto não está disponível</td></tr>';
-			}
+			if(data.status == 'I') result+='<tr><td colspan="4" style="color:red">Este produto não está disponível</td></tr>';
 		}
 		$('#table').html(result);
 		$('#conteudo').addClass('content');
 	});
-	
 }
 
 $(document).ready(function(){
@@ -44,12 +39,8 @@ $(document).ready(function(){
 				$('#table').html('');
 				$('#conteudo').removeClass('content');
 			}
-			else if(i == 0){
-				show(true, 'http://192.168.1.109:8080/list');
-			}
-			else if(i != 0){
-				show(false, 'http://192.168.1.109:8080/product?chave='+i);
-			}
+			else if(i == 0) show(true, 'http://192.168.1.109:8080/list');
+			else if(i != 0) show(false, 'http://192.168.1.109:8080/product?chave='+i);
 		});
 	});	
 	/*
