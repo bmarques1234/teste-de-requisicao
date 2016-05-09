bb45d12f58f2e5ebcd48715431a7eb0750bd5c47
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-05-09 10:21:26 -0300

    refatoramento

--- app/index/javascript.js
+++ app/index/javascript.js
@@ -9,42 +9,46 @@ function att(data){
 	$('#selecionar').html(out);
 }
 
+function get(allData,url){
+	$.getJSON(url, function(data){
+		var result='';
+		result+='<tr><th>Nome</th>'+'<th>Valor</th>'+'<th>Status</th>'+'<th>Estoque</th></tr>';
+		if(allData==true){
+			att(data);
+			for (var c=0;c<data.length;c++){
+				if(data[c].status == 'I') result+='<tr style="color:red">';
+				else result+='<tr>';
+				result+='<td>'+data[c].nome+'</td>'+'<td>R$ '+data[c].valor+'</td>';
+				result+='<td>'+data[c].status+'</td>'+'<td>'+data[c].estoque+'</td>'+'</tr>';
+			}
+		}
+		else{
+			result+='<tr>'+'<td>'+data.nome+'</td>'+'<td>R$ '+data.valor+'</td>';
+			result+='<td>'+data.status+'</td>'+'<td>'+data.estoque+'</td>'+'</tr>';
+			if(data.status == 'I'){
+				result+='<tr><td colspan="4" style="color:red">Este produto não está disponível</td></tr>';
+			}
+		}
+		$('#table').html(result);
+		$('#conteudo').addClass('content');
+	});
+	
+}
+
 $(document).ready(function(){
 	$.getJSON('http://192.168.1.109:8080/list', function(data){
 		att(data);
 		$('#submit').click(function(){
 			var i=$('#selecionar').val();
-			var result='';
-			if(i != 0){
-				var url='http://192.168.1.109:8080/product?chave='+i;
-				$.getJSON(url, function(data){
-					result+='<tr><th>Nome</th>'+'<th>Valor</th>'+'<th>Status</th>'+'<th>Estoque</th></tr>';
-					result+='<tr>'+'<td>'+data.nome+'</td>'+'<td>R$ '+data.valor+'</td>';
-					result+='<td>'+data.status+'</td>'+'<td>'+data.estoque+'</td>'+'</tr>';
-					if(data.status == 'I'){
-						result+='<tr><td colspan="4" style="color:red">Este produto não está disponível</td></tr>';
-					};
-					$('#table').html(result);
-					$('#conteudo').addClass('content');
-				});
-			}
-			else if(i == ''){
+			if(i == ''){
 				$('#table').html('');
 				$('#conteudo').removeClass('content');
 			}
 			else if(i == 0){
-				$.getJSON('http://192.168.1.109:8080/list', function(data){
-					att(data);
-					result+='<tr><th>Nome</th>'+'<th>Valor</th>'+'<th>Status</th>'+'<th>Estoque</th></tr>';
-					for (var c=0;c<data.length;c++){
-						if(data[c].status == 'I') result+='<tr style="color:red">'
-						else result+='<tr>'
-						result+='<td>'+data[c].nome+'</td>'+'<td>R$ '+data[c].valor+'</td>';
-						result+='<td>'+data[c].status+'</td>'+'<td>'+data[c].estoque+'</td>'+'</tr>';
-					};
-					$('#table').html(result);
-					$('#conteudo').addClass('content');
-				});
+				get(true, 'http://192.168.1.109:8080/list');
+			}
+			else if(i != 0){
+				get(false, 'http://192.168.1.109:8080/product?chave='+i);
 			}
 		});
 	});	
